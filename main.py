7497cc887161da99a9211cc286683c98c130a43c
radvilemaur <57565417+radvilemaur@users.noreply.github.com>
2020-01-29 13:00:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ from data import *
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
+data_gen_args = dict()
 myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
