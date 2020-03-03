05da0b378db9df5c9086abc9d945ee034f59c436
radvilemaur <57565417+radvilemaur@users.noreply.github.com>
2020-03-03 10:37:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from data import *
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-data_gen_args = dict()
+data_gen_args = dict() # leave dict empty; no data augmentation used for training; 
 myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True) 
 model.fit_generator(myGene,steps_per_epoch=300,epochs=5,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
+results = model.predict_generator(testGene,15,verbose=1) #change 30 to 15 for 15 test images in my data set
 saveResult("data/membrane/test",results)
