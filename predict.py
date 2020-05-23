07e4186f27afb29b45dffebcbed8ef4ea890984b
radvilemaur <57565417+radvilemaur@users.noreply.github.com>
2020-05-23 09:30:40 +0100

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,8 @@
+
+from model import *
+from data import *
+testGene = testGenerator("data/membrane/test")
+model = unet()
+model.load_weights("unet_membrane.hdf5")
+results = model.predict_generator(testGene,30,verbose=1)
+saveResult("data/membrane/test",results)
